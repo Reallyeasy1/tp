2d33a5cb6715a654ded061fb4132562c795e00a2
Reallyeasy1 <loyongzhe@gmail.com>
2024-10-27 18:06:13 +0800

    Fix minor tutorial bug

--- src/main/java/seedu/address/model/tut/Tutorial.java
+++ src/main/java/seedu/address/model/tut/Tutorial.java
@@ -385,7 +385,7 @@ public abstract class Tutorial {
         }
         @Override
         public String toString() {
-            return tutName + ": Tutorial " + tutorialId;
+            return tutName + ": " + tutorialId;
         }
     }
 }
